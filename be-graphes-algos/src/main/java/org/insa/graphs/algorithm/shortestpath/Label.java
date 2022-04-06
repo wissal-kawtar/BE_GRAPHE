28b2c412284dbb1b8a7c1b2e8d9786fe742d98a4
wissalkawtar <kawtaressgheir17@gmail.com>
2022-04-06 10:44:54 +0200

    debut seance Djikstra

---
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
@@ -0,0 +1,17 @@
+package org.insa.graphs.algorithm.shortestpath;
+
+import org.insa.graphs.model.Node;
+
+public class Label {
+
+   private boolean marque;
+   private Node sommet;
+   private double cout;
+   private Node père ;
+   
+   public getCost() {
+	   
+   }
+
+	
+}
